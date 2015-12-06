b4a8ca0355a3ccdcd193b07a5c6588f5df2f4597
İrfan Evrens <irfanevrens@gmail.com>
2015-12-07 01:21:40 +0200

    Tiny changes.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,9 +3,14 @@
 namespace OkulBilisim\LocationBundle\Controller;
 
 use OkulBilisim\LocationBundle\Entity\Country;
+use OkulBilisim\LocationBundle\Entity\Province;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * @method RegistryInterface getDoctrine
+ */
 class DefaultController extends Controller
 {
     public function citiesAction($country)
@@ -13,19 +18,17 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         /** @var Country $country */
-        $country = $em->getRepository('OkulBilisimLocationBundle:Country')->find($country);
-        if (!$country) {
+        if ( ! $country = $em->getRepository('OkulBilisimLocationBundle:Country')->find($country)) {
             throw $this->createNotFoundException();
         }
 
-        $cities_array = [];
-        foreach ($country->getProvinces() as $city) {
-            $cities_array[] = [
-                'id' => $city->getId(),
-                'name' => $city->getName(),
-            ];
-        }
+        return JsonResponse::create($country->getProvinces()->map(function(Province $province) {
 
-        return JsonResponse::create($cities_array);
+            return [
+                'id' => $province->getId(),
+                'name' => $province->getName()
+            ];
+
+        })->toArray());
     }
 }
